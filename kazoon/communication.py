abff914b309b5fd2f5a5e18c0d34ef59175b584c
Osher Solimany <solimani.osher@gmail.com>
2022-10-31 15:05:14 +0200

    send_email_gmail upload

---
+++ kazoon/communication.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def send_email_gmail(from_name, to_mail, subject, content, user, password):
+    email = EmailMessage()
+    email["from"] = from_name
+    email["to"] = to_mail
+    email["subject"] = subject
+    email.set_content(content)
+
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(user, password)
+        smtp.send_message(email)
+
+
+send_email_gmail("Osher", "solimani.osher@gmail.com", "Hello", "My name is osher", "solimani.osher@gmail.com",
+                 "sxzxqpqigpuwdwoq")
